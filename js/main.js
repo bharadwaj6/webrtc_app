95a00ccdb066e917628f4b11e07db01539f60af5
bharadwaj6 <krishna.bharadwaj6@gmail.com>
2014-04-02 03:33:10 +0530

    added effects, video configs

--- js/main.js
+++ js/main.js
@@ -3,16 +3,71 @@ navigator.getUserMedia  = navigator.getUserMedia ||
                           navigator.mozGetUserMedia ||
                           navigator.msGetUserMedia;
 
-var video = document.querySelector('video');
+var video = document.querySelector('video#video1');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+var localMediaStream = null;
+
+function snapshot() {
+	if (localMediaStream) {
+		ctx.drawImage(video, 0, 0);
+		document.querySelector('img').src = canvas.toDataURL('image/webp');
+	}
+}
+
+video.addEventListener('click', snapshot, false);
+
+// Video Constraints
+var hdConstraints = {
+	video: {
+		mandatory: {
+			minWidth: 1280,
+			minHeight: 720
+		}
+	},
+	audio: true
+};
+
+var vgaConstraints = {
+  video: {
+    mandatory: {
+      maxWidth: 640,
+      maxHeight: 360
+    }
+  },
+  audio: true
+};
+
+var idx = 0;
+var filters = ['grayscale', 'sepia', 'blur'];
+
+function success(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    localMediaStream = stream;
+}
 
 function errorCallback(e) {
 	console.log("navigator.getUserMedia error: ", e);
 }
 
+function fallback(error) {
+	video.src = 'fallbackvideo.webm';
+}
+
+function changeFilter(e) {
+	var el = e.target;
+	el.className = "";
+	var effect = filters[idx++ % filters.length];
+	if (effect) {
+		el.classList.add(effect);
+	}
+}
+
+document.querySelector('video#video1').addEventListener(
+	'click', changeFilter, false);
+
 if (navigator.getUserMedia) {
-  navigator.getUserMedia({audio: true, video: true}, function(stream) {
-    video.src = window.URL.createObjectURL(stream);
-  }, errorCallback);
+	navigator.getUserMedia(vgaConstraints, success, errorCallback);
 } else {
-  video.src = 'somevideo.webm'; // fallback.
+	fallback();
 };
